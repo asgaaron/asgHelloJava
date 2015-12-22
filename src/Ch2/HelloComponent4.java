8aeb3de0254215f28eb4d056ef8cf404afe570a7
Aaron Gardner <asgaaron@gmail.com>
2015-12-22 15:34:15 -0700

    removed unused imports

--- src/Ch2/HelloComponent4.java
+++ src/Ch2/HelloComponent4.java
@@ -3,7 +3,6 @@ package Ch2;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
-import java.awt.PopupMenu;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
@@ -74,7 +73,10 @@ class HelloComponent4 extends JComponent implements MouseMotionListener, ActionL
 	@Override
 	public void actionPerformed(ActionEvent e)
 	{
-		if (e.getSource() == button)
+		if (e.getSource() != button)
+		{
+		}
+		else
 			changeColor();
 	}
 
