c8e1d83241aa1ca03ca3046bc56e10408bd270ca
Aaron Gardner <asgaaron@gmail.com>
2015-12-28 09:40:56 -0700

    Added animal.java as examples of static classes and inner classes

---
+++ src/Ch6/Animal.java
@@ -0,0 +1,30 @@
+package Ch6;
+
+public class Animal
+{
+	int size;
+
+	static class MigrationPattern
+	{
+//		...
+	}
+
+	void performBehavior(final boolean nocturnal)
+	{
+		class Brain
+		{
+			int size;
+
+			void sleep()
+			{
+				if (nocturnal)
+				{
+//					...
+				}
+			}
+//		...
+		}
+//		...
+	}
+//	...
+}
